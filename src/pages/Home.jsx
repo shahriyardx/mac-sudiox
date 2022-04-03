0eccfe077fb52c5da9061fc4f92d053b4e4d6fcf
Shahriyar <mdshahriyaralam552@gmail.com>
2022-04-04 02:06:28 +0600

    Separate sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header/Header'
-import Hero from '../components/Hero/Hero'
+import Hero from '../sections/Hero/Hero'
 
 const Home = () => {
   return (
