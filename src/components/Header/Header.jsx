a3c845d27cb5534670b25a5d89d5aca84f6dfa6d
Shahriyar <mdshahriyaralam552@gmail.com>
2022-04-04 11:39:25 +0600

    Using semantic tags for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,20 @@ import NavLink from './NavLink'
 
 const Header = () => {
   return (
-    <div className='py-3'>
+    <header className='py-3'>
       <Container className='flex flex-col items-center justify-center gap-3 sm:gap-0 sm:flex-row'>
          <div className="text-2xl font-bold uppercase">
             MAC STUDIO
          </div>
 
-         <div className='flex items-center gap-3 sm:ml-auto'>
+         <nav className='flex items-center gap-3 sm:ml-auto'>
            <NavLink to="/" text="Home"/>
            <NavLink to="/reviews" text="Reviews"/>
            <NavLink to="/dashboard" text="Dashboard"/>
            <NavLink to="/blog" text="Blog"/>
-         </div>
+         </nav>
       </Container>
-    </div>
+    </header>
   )
 }
 
