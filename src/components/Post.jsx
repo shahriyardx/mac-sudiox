af98d87e8be36ea57f6e1665ac9fd36d38b72026
Shahriyar <mdshahriyaralam552@gmail.com>
2022-04-04 11:38:41 +0600

    Fix font styling for post header

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ export const Post = ({ post }) => {
 
   return (
     <div className='p-4 rounded-md bg-zinc-600/20'>
-      <h2 className='mb-2 text-lg'>{title}</h2>
+      <h2 className='mb-2 text-xl font-semibold'>{title}</h2>
       <p className='text-slate-400'>
         {content}
       </p>
