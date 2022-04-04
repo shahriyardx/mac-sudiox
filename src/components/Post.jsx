a970a1bd861aeb4eb496061d238ced0ad459c7ab
Shahriyar <mdshahriyaralam552@gmail.com>
2022-04-04 11:39:54 +0600

    Using semantic tags for post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,11 +4,11 @@ export const Post = ({ post }) => {
   const { title, content } = post
 
   return (
-    <div className='p-4 rounded-md bg-zinc-600/20'>
+    <article className='p-4 rounded-md bg-zinc-600/20'>
       <h2 className='mb-2 text-xl font-semibold'>{title}</h2>
       <p className='text-slate-400'>
         {content}
       </p>
-    </div>
+    </article>
   )
 }
